e1e778f536a35ab4cc2dc577c2a495e9a1282f2e
Maarten Sebregts <msebregts@ignitioncomputing.com>
2026-02-12 10:40:36 +0100

    Add authors and copyright

--- docs/conf.py
+++ docs/conf.py
@@ -3,10 +3,15 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import importlib.metadata
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'sphinx-ymmsl'
+author = "Iris van der Werf, Maarten Sebregts"
+copyright = "2026-%Y, ITER Organization"
+version = release = importlib.metadata.version(project)
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
