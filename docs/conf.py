8b073d75ccb70cc711c7977682d110346404bb0d
Maarten Sebregts <msebregts@ignitioncomputing.com>
2026-03-06 16:06:56 +0100

    Add ReadTheDocs configuration

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ import importlib.metadata
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'sphinx-ymmsl'
+project = "sphinx-ymmsl"
 author = "Iris van der Werf, Maarten Sebregts"
 copyright = "2026-%Y, ITER Organization"
 version = release = importlib.metadata.version(project)
@@ -18,15 +18,15 @@ version = release = importlib.metadata.version(project)
 
 extensions = [
     "sphinx_ymmsl",
+    "sphinx_rtd_theme",
 ]
 
 # templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
